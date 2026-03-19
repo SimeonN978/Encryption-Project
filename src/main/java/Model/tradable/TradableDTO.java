817f29d48c3acc2909b48451aeb510a4d4ca6140
Daniel-Carbajal <danbusiness1022@gmail.com>
2026-03-18 21:05:49 -0500

    Refactored Project Structure and import statements

--- src/main/java/Model/tradable/TradableDTO.java
+++ src/main/java/Model/tradable/TradableDTO.java
@@ -1,7 +1,7 @@
-package Java.Model.tradable;
+package Model.tradable;
 
-import Java.Model.productbook.BookSide;
-import Java.Model.price.Price;
+import Model.productbook.BookSide;
+import Model.price.Price;
 
 public record TradableDTO(String user, String product, Price price, int originalVolume, int remainingVolume, int cancelledVolume, int filledVolume, BookSide side, String tradableId) {
 
