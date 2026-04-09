55136bb4435dbfc6b493a0b7b41715f5bbf6765c
Daniel-Carbajal <danbusiness1022@gmail.com>
2026-04-08 22:20:38 -0500

    Created MFAController and MFAService instances. Register MFA routes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 import Controller.AuthenticationController;
+import Controller.Controller;
 import Controller.DashBoardController;
-import Controller.RateLimiter;
+import Controller.MFAController;
+import Service.MFAService;
+import Service.RateLimiter;
 import Model.account.AccountStore;
 import Service.UserService;
 import Service.SignUpValidationService;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import static spark.Spark.*;
 
@@ -16,8 +21,10 @@ public class Main {
         //TODO: Project code here
         AccountStore accountStore = new AccountStore();
 
+        // Create Services
         UserService authenticationService = new UserService(accountStore);
         SignUpValidationService signUpValidationService = new SignUpValidationService(accountStore);
+        MFAService mfaService = new MFAService();
 
         RateLimiter rateLimiter = new RateLimiter(5, Duration.ofMinutes(15)); // 5 attempts per minute until hard throttle
 
@@ -28,6 +35,9 @@ public class Main {
         // 3. Define routes and start server
         AuthenticationController authRoutes = new AuthenticationController(authenticationService, signUpValidationService,rateLimiter);
         authRoutes.register();
+
+        MFAController mfaRoutes = new MFAController(mfaService);
+        mfaRoutes.register();
 
         DashBoardController dashRoutes = new DashBoardController(authenticationService);
         dashRoutes.register();
